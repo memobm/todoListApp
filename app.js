1c8309d43d7ab5803b983d4e1d12e2f7d58f2981
Guillermo Barragan <memobm09@gmail.com>
2019-03-30 01:30:10 -0700

    Add a delete feature with its styles

--- app.js
+++ app.js
@@ -13,22 +13,33 @@ function todoSubmit(event) {
 function createListItem(text) {
     // Creating an li html tag
     const liEl = document.createElement('li');
+    const spanEl = document.createElement('span');
+
+    spanEl.innerHTML = '&times';
+    spanEl.classList.add('delete-hide');
 
     // Set to add the input value later on
-    liEl.innerHTML = `${text}<span class='delete'>&times</span>`;
+    liEl.textContent = text;
     liEl.classList.add('todo-item');
 
+    liEl.appendChild(spanEl);
     return liEl;
 }
 
 function doneToggle() {
-    ulEl.addEventListener('click', function(event) {
-
+    ulEl.addEventListener('click', function (event) {
         // Checks to see if the target contains a todo-item class
         if (event.target.classList.contains('todo-item')) {
             event.target.classList.toggle('done');
-            // Removes the items
-            ulEl.removeChild(event.target);
+
+            if (event.target.classList.contains('done')) {
+                newSpanEl = event.target.children;
+                for (item of newSpanEl) {
+                    item.classList.add('delete');
+                    item.classList.remove('delete-hide');
+                    item.addEventListener('click', () => ulEl.removeChild(event.target))
+                }
+            }
         }
     })
 }
